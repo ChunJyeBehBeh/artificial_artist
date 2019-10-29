0e5d538349639cb9d1464c6e74dfcae01d897a49
Chun Jye <chloridenitride@hotmail.com>
2019-10-29 10:57:49 +0800

    update

--- combined_write_simulation.py
+++ combined_write_simulation.py
@@ -59,33 +59,12 @@ s6 = sg.Segment(0,0,ey,0)
 s7 = sg.Segment(2,1.00,ex,0)
 s8 = sg.Segment(0,0,ey,0)
 s9 = sg.Segment(2,1.00,ex,0)
+s10 = sg.Segment(2,1.0,ez,0)
 
 #Organize segments into a list, order matters!
-segments = [s1,s2,s3,s4,s5,s6,s7,s8,s9]
+segments = [s1,s2,s3,s4,s5,s6,s7,s8,s9]             # <- fix
 #Organize zero config angles, order matters!
-Q = [0,0,0,0,0,0,0,0,0]
-#Unit vectors in x,y,z direction for convenience 
-ex = np.matrix([[1],[0],[0]])
-ey = np.matrix([[0],[1],[0]])
-ez = np.matrix([[0],[0],[1]])
-
-#############################
-#Create the segments that make up robot. Segment is initialized in the following form
-# sg.Segment(type of joint, length, unit vector, zero configuration angle)
-s1 = sg.Segment(0,0,ez,0)     # <-- Base servo
-s2 = sg.Segment(2,1.00,ez,0)  # <- link
-s3 = sg.Segment(0,0,ey,0)
-s4 = sg.Segment(2,1.00,ez,0)
-s5 = sg.Segment(2,2.00,ex,0)
-s6 = sg.Segment(0,0,ey,0)
-s7 = sg.Segment(2,1.00,ex,0)
-s8 = sg.Segment(0,0,ey,0)
-s9 = sg.Segment(2,1.00,ex,0)
-
-#Organize segments into a list, order matters!
-segments = [s1,s2,s3,s4,s5,s6,s7,s8,s9]
-#Organize zero config angles, order matters!
-Q = [0,0,0,0,0,0,0,0,0]
+Q = [0,0,0,0,0,0,0,0,0]                             # <- fix
 
 #construct robot arm from segment list and zero config list
 r1 = ra.RobotArm(segments,Q)
